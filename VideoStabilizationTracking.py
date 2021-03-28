462f709b636836dae13e33999f0786ad579bf062
Abhijeet Kulkarni <amkulk@udel.edu>
2021-03-28 17:06:30 -0400

    commented

--- VideoStabilizationTracking.py
+++ VideoStabilizationTracking.py
@@ -2,16 +2,13 @@
 """
 Created on Sun Mar 28 12:40:56 2021
 
-@author: amk17
+@author: amkulk@udel.edu (Abhijeet Kulkarni)
 """
 
-# from imutils.video import VideoStream
-# from imutils.video import FPS
-# import imutils
-import time
 import cv2
+filepath = 'wolf.mp4'
 
-
+# helping functions
 def DrawBoundingBox(frame,bbox,color= (255,0,0),thickness=2):
     (x, y, w, h) = [int(d) for d in bbox]
     cv2.rectangle(frame, (x,y), (x+w,y+h),color,thickness)
@@ -25,10 +22,8 @@ def GetBoxCenter(bbox):
 def CropFrame(frame,composition=None,currentObjCenter=None):
     global objCenter
     if composition is not None:
-        
-        
         (x, y, w, h) = [int(d) for d in composition]
-        framepadded = cv2.copyMakeBorder(frame,h,h,w,w,cv2.BORDER_CONSTANT,None,0)
+        framepadded = cv2.copyMakeBorder(frame,h,h,w,w,cv2.BORDER_CONSTANT,None,0) # without padding overflows
         dx,dy=currentObjCenter[0]-objCenter[0],currentObjCenter[1]-objCenter[1]
         framecropped=framepadded[h+y+dy:h+y+h+dy,w+x+dx:w+x+dx+w]
     return framecropped
@@ -41,16 +36,14 @@ def DisplayImage(frame):
     return key
 
 def WriteVideo(frame,out):
-    # Define the codec and create VideoWriter object
     global videoFPS
-    dim = frame.shape[:2]
-
+    dim = frame.shape[:2] #[height,width]
     if out is None:
         out = cv2.VideoWriter("TrackedVideo.mp4",cv2.VideoWriter_fourcc(*'XVID'), videoFPS, (dim[1],dim[0]))
     out.write(frame)
     return out
 
-video=cv2.VideoCapture('wolf.mp4')
+video=cv2.VideoCapture(filepath)
 videoFPS= video.get(cv2.CAP_PROP_FPS)
 OPENCV_OBJECT_TRACKERS = {
           "csrt": cv2.TrackerCSRT_create(),
@@ -58,7 +51,7 @@ OPENCV_OBJECT_TRACKERS = {
           "mil": cv2.TrackerMIL_create(),
     }
 
-tracker = OPENCV_OBJECT_TRACKERS['csrt']
+tracker = OPENCV_OBJECT_TRACKERS['csrt'] 
 tracking = None
 composition = None
 success = True
@@ -67,14 +60,11 @@ videowriter = None
 
 while True:
     video_incoming, frame = video.read()
-    
-    
     if video_incoming:
-
         if tracking is not None:
             (success,tracking) = tracker.update(frame)
             if success:
-                DrawBoundingBox(frame, tracking)
+                DrawBoundingBox(frame, tracking) #comment this for suppressing blue box
                 frameCropped = CropFrame(frame,composition,GetBoxCenter(tracking))
                 key = DisplayImage(frameCropped)
                 
@@ -82,13 +72,10 @@ while True:
             else:
                 key = DisplayImage(frame)
         else:
-            
+
             key = DisplayImage(frame)
-        
-        
-        
-        
-        if key == ord("s") or success == False:
+            
+        if key == ord("s") or success == False: #select ROI
             tracking = cv2.selectROI("Select ROI",frame,fromCenter=False,showCrosshair=True)
             tracker.init(frame,tracking)
             cv2.destroyAllWindows()
@@ -98,11 +85,11 @@ while True:
                 composition = cv2.selectROI("Select Composition",frame,fromCenter=False,showCrosshair=True)
                 cv2.destroyAllWindows()
         
-        elif key == ord("q"):
+        elif key == ord("q"): #stop video
             break
         
-        elif key == ord("f"):
-            _=[video.read() for i in range (60)]
+        elif key == ord("f"): #skipping 1 sec
+            _=[video.read() for i in range (1000/videoFPS)]
         
         
             
